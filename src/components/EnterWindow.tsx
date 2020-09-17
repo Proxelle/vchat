676a93b3fa98d636be15682a8cf1b1ee09ee2818
proxelle <proxelle@inbox.ru>
2020-09-17 08:33:10 +0300

    first commit

---
+++ src/components/EnterWindow.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default () => {
+    return (
+        <div className="enter">
+            <div className="toggle-btns">
+                <div className="toggle-btns-create">Create</div>
+                <div className="toggle-btns-enter">Enter</div>
+            </div>
+            <div className="create-room">create-room</div>
+            <div className="enter-room">enter-room</div>
+        </div>
+    )
+}
